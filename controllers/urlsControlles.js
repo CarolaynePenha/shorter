fc94f5c61a6648dac75d2bbe2e35a7897dcf3a10
Carolayne <carolaynepenha@gmail.com>
2023-12-09 13:11:11 -0300

    feat: endpoints to get link by short url or id, and to delete url by id.

--- controllers/urlsControlles.js
+++ controllers/urlsControlles.js
@@ -2,19 +2,88 @@ import connection from "../db.js";
 import { nanoid } from "nanoid";
 
 export async function shortUrl(req, res) {
-  const url = req.body;
+  const { url } = req.body;
+
   const shortLink = nanoid();
   const { userId } = res.locals;
   console.log("userId: ", userId);
+  console.log("url: ", url);
 
   try {
     const queryLinks = await connection.query(
-      `INSERT INTO links ("completeLink","shortLink","userId")
+      `INSERT INTO links (url,"shortUrl","userId")
       VALUES ($1,$2,$3)
       `,
       [url, shortLink, userId.userId]
     );
     res.status(201).send(shortLink);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function getLink(req, res) {
+  const { id } = req.params;
+  try {
+    const queryLink = await connection.query(
+      `SELECT id,url,"shortUrl" FROM links 
+      WHERE id=$1
+      `,
+      [id]
+    );
+    if (queryLink.rowCount === 1) {
+      return res.status(201).send(queryLink.rows[0]);
+    }
+    return res.sendStatus(404);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function getShortUrl(req, res) {
+  const { shortUrl } = req.params;
+
+  try {
+    const queryUrlSelect = await connection.query(
+      `SELECT url FROM links 
+      WHERE "shortUrl"=$1 
+      `,
+      [shortUrl]
+    );
+
+    if (queryUrlSelect.rowCount === 1) {
+      const queryUpdateVisits = await connection.query(
+        `UPDATE links 
+        SET "urlVisits"="urlVisits"+1
+        WHERE "shortUrl"=$1 
+        `,
+        [shortUrl]
+      );
+      return res.redirect(queryUrlSelect.rows[0].url);
+    }
+    return res.sendStatus(404);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function deleteLink(req, res) {
+  const { id } = req.params;
+  const { userId } = res.locals;
+  try {
+    const queryDelete = await connection.query(
+      `DELETE FROM links
+      WHERE id=$1 AND "userId"=$2
+      `,
+      [id, userId.userId]
+    );
+    if (queryDelete.rowCount === 1) {
+      return res.sendStatus(204);
+    }
+    return res.sendStatus(404);
   } catch (err) {
     console.error(err);
     res.sendStatus(500);
--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { urlSchema } from "../middlewares/urlMiddlewares.js";
 import tokenValidation from "../middlewares/tokenValidation.js";
-import { shortUrl } from "../controllers/urlsControlles.js";
+import {
+  deleteLink,
+  getLink,
+  getShortUrl,
+  shortUrl,
+} from "../controllers/urlsControlles.js";
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", tokenValidation, urlSchema, shortUrl);
+urlRouter.get("/urls/:id", getLink);
+urlRouter.get("/urls/open/:shortUrl", getShortUrl);
+urlRouter.delete("/urls/:id", tokenValidation, deleteLink);
 
 export default urlRouter;
